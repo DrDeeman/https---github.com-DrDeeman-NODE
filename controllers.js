cc54e3832ffacd6852902d7ab475c5577b842729
My Name <stalkerdrdeeman@gmail.com>
2020-12-12 20:35:16 +0300

    node3

--- controllers.js
+++ controllers.js
@@ -1,63 +1,93 @@
-const fileModule= require('fs');
+const fileModule = require('fs');
 const pathModule = require('path');
 const { request, response } = require('express');
 let arrayJSON;
 
 
-function readF(request,response,type){
-    const fileProduct = pathModule.resolve(__dirname,'Products.json');
-    let promise = new Promise((resolve,reject)=>{
-        fileModule.readFile( fileProduct ,'utf-8',
-        (error, data) => {
-            if (error) {
-              reject(new Error(error.stack));
-            } else {
-                if(type=='add' || type=='edit' || type=='delete')resolve(JSON.parse(data));
-                if(type=='load') resolve(data);
-            }
-        });
+function readF(request, response, type) {
+    const fileProduct = pathModule.resolve(__dirname, 'Products.json');
+    let promise = new Promise((resolve, reject) => {
+        fileModule.readFile(fileProduct, 'utf-8',
+            (error, data) => {
+                if (error) {
+                    reject(new Error(error.stack));
+                } else {
+                    if (type == 'add' || type == 'edit' || type == 'delete') {
+                        resolve(JSON.parse(data));
+                    }
+                    if (type == 'load') {
+                        resolve(data);
+                    }
+                }
+            });
     });
     promise.then(
-        resolve=(data)=>{
-            switch(type){
+        resolve = (data) => {
+            switch (type) {
                 case 'add':
-         data.push(request.body);
-        fileModule.writeFile( fileProduct ,JSON.stringify(data),function(error){if(error)console.log(error);});
-        response.sendStatus(200);
-        break;
-     case 'edit':
-        data[request.body.Number-1].Comics = request.body.Comics;
-        if(Object.keys(request.body).length==3) data[request.body.Number-1].Count = request.body.Count;
-         fileModule.writeFile( fileProduct ,JSON.stringify(data),function(error){if(error)console.log(error);});
-         response.sendStatus(200);
-         break;
-         case 'delete':
-             data.splice(request.body.Number-1,1);
-             fileModule.writeFile( fileProduct ,JSON.stringify(data),function(error){if(error)console.log(error);});
-             response.sendStatus(200);
-             break;
-     case 'load':
-        response.send(data);
-        break;
+                    data.push(request.body);
+                    fileModule.writeFile(fileProduct, JSON.stringify(data),
+                        function (error) {
+                            if (error) {
+                                console.log(error);
+                                response.sendStatus(500);
+                            }
+                        });
+                    response.sendStatus(200);
+                    break;
+                case 'edit':
+                    data[request.body.Number - 1].Comics = request.body.Comics;
+                    if (Object.keys(request.body).length == 3) {
+                        data[request.body.Number - 1].Count = request.body.Count;
+                    }
+                    fileModule.writeFile(fileProduct, JSON.stringify(data),
+                        function (error) {
+                            if (error) {
+                                console.log(error);
+                                response.sendStatus(500);
+                            }
+                        });
+                    response.sendStatus(200);
+                    break;
+                case 'delete':
+                    data.splice(request.body.Number - 1, 1);
+                    fileModule.writeFile(fileProduct, JSON.stringify(data),
+                        function (error) {
+                            if (error) {
+                                console.log(error);
+                                response.sendStatus(500);
+                            }
+                        });
+                    response.sendStatus(200);
+                    break;
+                case 'load':
+                    response.send(data);
+                    break;
             }
-    }
+        }
     )
 }
-exports.appComics = function(request,response){
-    if(request.body)readF(request,response,'add'); 
+exports.appComics = function (request, response) {
+    if (request.body) {
+        readF(request, response, 'add');
+    }
 }
 
-exports.editComics=function(request,response){
-    if(request.body)  readF(request,response,'edit');
+exports.editComics = function (request, response) {
+    if (request.body) {
+        readF(request, response, 'edit');
+    }
 }
 
-exports.deleteComics = function(request,response){
-    if(request.body.Number!='')readF(request,response,'delete');
+exports.deleteComics = function (request, response) {
+    if (request.body.Number != '') {
+        readF(request, response, 'delete');
+    }
 }
-exports.renderComics = function(request,response){
-  response.sendFile(__dirname+'/main/main.html');
+exports.renderComics = function (request, response) {
+    response.sendFile(__dirname + '/main/main.html');
 }
 
-exports.loadComics = function(request,response){
-    readF(request,response,'load');
+exports.loadComics = function (request, response) {
+    readF(request, response, 'load');
 }
--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const http = require('express');
 const bodyParser = require("body-parser");
 const port = 3000;
 const app = http();
-const urlencodedParser = bodyParser.urlencoded({extended: true});
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const mainRouter = http.Router();
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const mainController = require(__dirname+"/controllers.js");
-mainRouter.use('/postcomics',mainController.appComics);
-mainRouter.use('/deletecomics',mainController.deleteComics);
-mainRouter.use('/form.html',mainController.loadComics);
-mainRouter.use('/main.html',mainController.renderComics);
-mainRouter.use('/postcomicsredact',mainController.editComics);
-app.use('/',mainRouter);
+const mainController = require(__dirname + "/controllers.js");
+mainRouter.use('/postcomics', mainController.appComics);
+mainRouter.use('/deletecomics', mainController.deleteComics);
+mainRouter.use('/form.html', mainController.loadComics);
+mainRouter.use('/main.html', mainController.renderComics);
+mainRouter.use('/postcomicsredact', mainController.editComics);
+app.use('/', mainRouter);
 
 
 //mainRouter.get('/form.html', (request, response) => {
-   // response.sendFile(__dirname+'/template/form.html')
+// response.sendFile(__dirname+'/template/form.html')
 //});
 
 
@@ -24,6 +24,6 @@ app.use('/',mainRouter);
 
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
- 
+
     console.log(`Server listening on port ${server.address().port}`);
 });
