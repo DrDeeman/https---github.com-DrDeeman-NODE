075bd71bec19a185b0be0250a1cdf8ab16afb850
My Name <stalkerdrdeeman@gmail.com>
2020-12-18 16:32:14 +0300

    fg

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const pathM = require('path');
 
 module.exports={
     mode:'development',
-    entry:pathM.resolve(__dirname,'index.js'),
+    entry:pathM.resolve(__dirname+'/main/static/scripts.js'),
     output:{
         path:pathM.resolve(__dirname,'public'),
         filename:'main.js'
