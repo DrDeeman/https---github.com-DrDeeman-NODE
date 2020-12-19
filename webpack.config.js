12d18973dc26f199ca9c78aa5740e10da7d9e9d5
My Name <stalkerdrdeeman@gmail.com>
2020-12-19 20:01:08 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 const pathM = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports={
     mode:'development',
+    devtool:'eval-cheap-module-source-map',
     entry:pathM.resolve(__dirname+'/src/scripts/scripts.js'),
     output:{
         path:pathM.resolve(__dirname,'src/public'),
+        publicPath:__dirname+'/src/public',
         filename:'main.js'
     },
     
     
     module:{
          rules:[
-             {
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader"
+                },
+              },
+               {
                 test: /\.css$/,
                 use: [
+                  'postcss-loader',
                     {
                     loader:'file-loader',
                     options:{
@@ -26,7 +38,22 @@ module.exports={
 
     },
     plugins:[
-        new CleanWebpackPlugin()
-    ]
+        new CopyPlugin({
+            patterns: [
+              {
+                from: pathM.resolve(__dirname +"/src/scripts/styles.css"),
+                to: pathM.resolve(__dirname+'/src/public')
+              }
+            ]
+        }),
+        new CleanWebpackPlugin({ exclude:[".gitkeep"]}),
+        new HtmlWebpackPlugin()
+    ],
+    devServer: {  // configuration for webpack-dev-server
+     contentBase:pathM.resolve(__dirname+'/src/scripts'),  //source of static assets
+     publicPath:pathM.resolve(__dirname+'/src/public'),
+      open: true,
+      port: 3000, // port to run dev-server
+  } 
 }
 
